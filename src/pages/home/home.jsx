87283d1e91aa0e539265f5ecbe9dacdee8b14e47
radhaya9av <radhaya9av@gmail.com>
2024-07-11 22:06:45 +0530

    integratingNASAmarsAPI

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import { formatDate } from '../../helpers';
+
 import Info from "../../components/Info"
 import Unit from "../../components/Unit"
 import WeatherData from "../../components/WeatherData"
 import "../../styles/Home.scss"
 
 function Home() {
+  const [loading, setLoading] = useState(true);
+  const [weather, setWeather] = useState([]);
+  const [selectedSol, setSelectedSol] = useState();
+  const [metric, setMetric] = useState(true);
+  console.log(weather);
+
+  const apiKey = import.meta.env.VITE_MARS_API_KEY;
+  const apiUrl = import.meta.env.VITE_MARS_API_URL;
+
   return (
     <>
       <div className = "MarsWeather">
