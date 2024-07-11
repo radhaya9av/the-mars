0c5eea16e9ef333fa7a9199677e47bc767776809
radhaya9av <radhaya9av@gmail.com>
2024-07-11 08:11:10 +0530

    fileStructure

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <>
+      <div>
+        <h1 className='main-title'>
+            Latest weather at Elysium Plantitia
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
