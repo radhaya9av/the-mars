17ca305d116ee053ca8a42e326ca5e57c3f97684
radhaya9av <radhaya9av@gmail.com>
2024-07-11 22:09:54 +0530

    IntegratingFunctionality

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -16,6 +16,28 @@ function Home() {
   const apiKey = import.meta.env.VITE_MARS_API_KEY;
   const apiUrl = import.meta.env.VITE_MARS_API_URL;
 
+  useEffect(() => {
+    const fetchFromAPI = async () => {
+      const weather = await (await fetch(apiUrl)).json();
+      const marsWeather = weather.sol_keys.map((solKey) => {
+        return {
+          sol: solKey,
+          maxTemp: weather[solKey].AT?.mx || 'No data',
+          minTemp: weather[solKey].AT?.mn || 'No data',
+          windSpeed: Math.round(weather[solKey].HWS?.av || 0),
+          windDirectionDegrees:
+            weather[solKey].WD?.most_common?.compass_degrees || 0,
+          date: formatDate(new Date(weather[solKey].First_UTC)),
+        };
+      });
+      setWeather(marsWeather);
+      setSelectedSol(marsWeather.length - 1);
+      setLoading(false);
+    };
+
+    fetchFromAPI();
+  }, []);
+
   return (
     <>
       <div className = "MarsWeather">
