855e17d01480ad17b44b7f9003f00c61a97e0342
radhaya9av <radhaya9av@gmail.com>
2024-07-12 16:41:15 +0530

    pageComponent

---
+++ src/components/Page.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Page } from '../styles/Page.styles'
+
+export default function ({ children }) {
+  return <Page>{children}</Page>
+}
---
+++ src/styles/Page.styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const Page = styled.main`
+  max-width: 960px;
+  margin: auto;
+  min-height: calc(100% - var(--footer-height));
+`
