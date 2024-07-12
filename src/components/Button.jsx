538dbdacab64d7f673617d7bbf6cfdf574d22932
radhaya9av <radhaya9av@gmail.com>
2024-07-12 16:28:04 +0530

    buttonComponent

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from 'arwes'
+
+export default function ButtonLink () {
+  
+  return (
+    <Link>
+      <Button>
+      </Button>
+    </Link>
+  )
+}
