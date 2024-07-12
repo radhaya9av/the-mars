17db0f61d4a40b8a377cece131405b1046ddb126
radhaya9av <radhaya9av@gmail.com>
2024-07-12 07:07:58 +0530

    infoStyling_JavaScript

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import "../styles/Info.scss"
+import { InfoStyles } from './Info.styles';
 
-function Info() {
-  
-    return(
-      <>
-        <div className = "InfoStyles">
-            <p>
-                InSight is taking daily weather measurements (temperature, wind, pressure)
-                on the surface of Mars at Elysium Planitia, a flat, smooth plain near
-                Mars’ equator.
-            </p>
-            <p>
-                This is only a part of InSight’s mission.{' '}
-                <a href='https://mars.nasa.gov/insight/mission/overview/'> Click here </a>
-                to find out more.
-            </p>
-        </div>
-      </>
-    )
-};
 
-export default Info
+const Info = () => (
+  <InfoStyles>
+    <p>
+      InSight is taking daily weather measurements (temperature, wind, pressure)
+      on the surface of Mars at Elysium Planitia, a flat, smooth plain near
+      Mars’ equator.
+    </p>
+    <p>
+      This is only a part of InSight’s mission.{' '}
+      <a href='https://mars.nasa.gov/insight/mission/overview/'>Click here</a>
+      to find out more.
+    </p>
+  </InfoStyles>
+);
+
+export default Info;
