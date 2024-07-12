29ece46296f6c1f6ce0d8be0a304b502b543fec8
radhaya9av <radhaya9av@gmail.com>
2024-07-12 16:38:57 +0530

    centeredComponent

---
+++ src/components/Centered.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Wrapper } from '../styles/Centered.styles'
+
+
+export default function Centered ({ children }) {
+  return <Wrapper>{children}</Wrapper>
+}
---
+++ src/styles/Centered.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 2em;
+  width: 100%;
+`
