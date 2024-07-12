2c3d9068f918b18ab4be6e69fffc477844be52be
radhaya9av <radhaya9av@gmail.com>
2024-07-12 16:23:13 +0530

    contentComponent

---
+++ src/components/Content.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Content } from '../styles/Content.styles'
+
+export default function ({ children }) {
+  return <Content>{children}</Content>
+}
---
+++ src/styles/Content.styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const Content = styled.div`
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+`
