8bd1d9bd5b05ea1a94543e5d84ddec776da815a0
radhaya9av <radhaya9av@gmail.com>
2024-07-13 15:45:10 +0530

    photosComponent

---
+++ src/pages/rovers/components/photos.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import { useParams } from 'react-router-dom'
+import Carousel from '../../../components/Carousel'
+import Centered from '../../../components/Centered'
+import PercentageLoader from '../../../components/PercentageLoader'
+import { useCameraStore } from '../../../stores/CamerasStore'
+import { FiCameraOff } from 'react-icons/fi'
+import Loader from '../../../components/Loader'
+import Error from '../../../components/Error'
+
+export const Wrapper = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  flex-direction: column;
+`
+
+export function Photos () {
+  const { rover, camera, sol } = useParams()
+  const [
+    { isLoading, progress, photos, error },
+    { fetchPhotos }
+  ] = useCameraStore()
+
+  useEffect(() => {
+    fetchPhotos({ rover, camera, sol })
+  }, [fetchPhotos, rover, camera, sol])
+
+  if (isLoading) {
+    return <Loader />
+  }
+
+  if (error) {
+    return <Error>{error}</Error>
+  }
+
+  if (!photos.length) {
+    return (
+      <Centered>
+        <FiCameraOff />
+        <div>No photos, try another camera</div>
+      </Centered>
+    )
+  }
+
+  if (progress < 1) {
+    return <PercentageLoader progress={progress} />
+  }
+
+  return (
+    <Wrapper>
+      <Carousel photos={photos} />
+    </Wrapper>
+  )
+}
