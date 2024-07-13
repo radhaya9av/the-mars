c6b6d4056982c81ab1d8a9c6b1fb259284bfa96d
radhaya9av <radhaya9av@gmail.com>
2024-07-13 15:49:57 +0530

    explorerPage

---
+++ src/pages/rovers/explorer.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useParams } from 'react-router-dom'
+import createDebug from 'debug'
+
+import Page from '../../components/Page'
+import Content from '../../components/Content'
+import Project from '../../components/Project'
+import Footer from '../../components/Footer'
+import ButtonLink from '../../components/ButtonLink'
+
+import Rover from './components/Rover'
+
+import { RoverContainer } from '../../stores/RoversStore'
+import { CameraContainer } from '../../stores/CamerasStore'
+
+export const debug = createDebug('mars:photos')
+
+const RoverMenu = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`
+
+export default function Photos () {
+  const { rover, camera } = useParams()
+
+  return (
+    <>
+      <Page>
+        <Content>
+          <>
+            <Project animate header='Rover'>
+              {anim => (
+                <RoverMenu>
+                  <ButtonLink to='/photos/perseverance'>Perseverance</ButtonLink>
+                  <ButtonLink to='/photos/curiosity'>Curiosity</ButtonLink>
+                  <ButtonLink to='/photos/opportunity'>Opportunity</ButtonLink>
+                  <ButtonLink to='/photos/spirit'>Spirit</ButtonLink>
+                </RoverMenu>
+              )}
+            </Project>
+          </>
+          {rover && (
+            <RoverContainer scope={rover}>
+              <CameraContainer scope={`${rover}  ${camera}`}>
+                <Rover />
+              </CameraContainer>
+            </RoverContainer>
+          )}
+        </Content>
+      </Page>
+      <Footer />
+    </>
+  )
+}
